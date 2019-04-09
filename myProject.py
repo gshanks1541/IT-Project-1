b4a90f88aa02feee56d9778fa4f36d44de682926
gshanks1541 <48245951+gshanks1541@users.noreply.github.com>
2019-04-10 09:28:20 +1000

    Update myProject.py

--- myProject.py
+++ myProject.py
@@ -1,7 +1,9 @@
 import kivy
 from kivy.app import App
-from kivy.uix.button import Label, Button
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 kivy.require("1.10.1")
 
@@ -13,29 +15,43 @@ class MyGrid(GridLayout):
 
         self.count = 0
         self.submit = Button(text="Scrape", font_size=40)
-        self.submit.bind(on_press=self.pressed)
+        self.submit.bind(on_press=self.scrape)
         self.add_widget(self.submit)
 
         self.count = 0
         self.submit = Button(text="Search", font_size=40)
-        self.submit.bind(on_press=self.pressed)
+        self.submit.bind(on_press=self.search)
         self.add_widget(self.submit)
 
         self.count = 0
         self.submit = Button(text="Export Data", font_size=40)
-        self.submit.bind(on_press=self.pressed)
+        self.submit.bind(on_press=self.export)
         self.add_widget(self.submit)
 
         self.count = 0
         self.submit = Button(text="Add New Feed", font_size=40)
-        self.submit.bind(on_press=self.pressed)
+        self.submit.bind(on_press=self.addfeed)
         self.add_widget(self.submit)
 
-    def pressed(self, instance):
+    def scrape(self, instance):
         self.count += 1
         upcount = self.count
-        print("Go button pressed ", upcount)
+        print("Scrape button pressed ", upcount)
 
+    def search(self, instance):
+        self.count += 1
+        upcount = self.count
+        print("Search button pressed ", upcount)
+
+    def export(self, instance):
+        self.count += 1
+        upcount = self.count
+        print("Export button pressed ", upcount)
+
+    def addfeed(self, instance):
+        self.count += 1
+        upcount = self.count
+        print("Add Feed button pressed ", upcount)
 
 class MyApp(App):
     def build(self):
