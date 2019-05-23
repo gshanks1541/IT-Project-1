6ab98f37ab897eeefd13e174e6aeaccfdb2ed6b8
gshanks1541 <48245951+gshanks1541@users.noreply.github.com>
2019-05-23 20:00:21 +1000

    Update twitterClient.py

--- twitterClient.py
+++ twitterClient.py
@@ -5,10 +5,10 @@ from tweepy import OAuthHandler
 
 def get_twitter_auth():
     try:
-        consumer_key = os.environ[nX7aJP4OUszK1dFgfm6BTZrXQ]
-        consumer_secret = os.environ[s1C56X7kGeSrum8M6VXs8IdfTMOfKDmpQnJTDodLcCr6kbv938]
-        access_token = os.environ[1128419970960936961-ocBanOhQYajRPcXQ3c1alFBf3BlanX]
-        access_secret = os.environ[elHOFXT79tht3RmP1V90lW6bKJhHxNjHHHDolFVyfadTO]
+        consumer_key = os.environ[]
+        consumer_secret = os.environ[]
+        access_token = os.environ[]
+        access_secret = os.environ[]
     except KeyError:
         sys.stderr.write("TWITTER_* environment variables not set\n")
         sys.exit(1)
